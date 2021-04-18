90febd3d1d5cffdf37224a9ece36defa37699587
Phillip Price <trafub@gmail.com>
2021-04-18 15:22:19 -0400

    Check associativity of a group
    This is the first draft of the program.  It takes in a CSV filename
    and checks whether the group that produced the associated group table
    is associative or not.

--- checkAssociativeProperty.m
+++ checkAssociativeProperty.m
@@ -1,10 +1,32 @@
-function checkAssociativeProperty (group_table)
+function checkAssociativeProperty (group_table_file)
   
   # Determines if the group that produces the provided group table
   # is associative, i.e. (a*b)*c = a*(b*c) for all a,b,c in the group.
   
+  # group_table should be the name of a CSV file
+  group_table = csvread(group_table_file)
   
+  group_size = length (group_table(1,:))
   
+  # TODO : Build in a test case here to ensure that the provided group table is 
+  # a square matrix
   
+  isAssociative = true
+  
+  for i = 1:group_size
+    for j = 1:group_size
+      for k = 1:group_size
+        if group_table(group_table(i,j),k) != group_table(i,group_table(j,k))
+          isAssociative = false
+        endif
+      endfor
+    endfor
+  endfor
+  
+  if isAssociative
+    printf("The group is associative\n")
+  else
+    printf("The group is not associative\n")
+  endif
   
 endfunction
