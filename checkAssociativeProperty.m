2a29747c01f40dfca7a968012be890df8bcf7054
Phillip Price <trafub@gmail.com>
2021-04-18 14:28:55 -0400

    Initial commit

---
+++ checkAssociativeProperty.m
@@ -0,0 +1,10 @@
+function checkAssociativeProperty (group_table)
+  
+  # Determines if the group that produces the provided group table
+  # is associative, i.e. (a*b)*c = a*(b*c) for all a,b,c in the group.
+  
+  
+  
+  
+  
+endfunction
