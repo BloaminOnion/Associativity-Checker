a180b6d34ea614f8aa69c1622d12e4fcd554bdf0
Phillip Price <trafub@gmail.com>
2021-04-19 11:33:39 -0400

    Ensure positive integer group table elements
    Because we use the group table entries as indices when checking the
    associative property for the group, we need the entries of the group to
    be positive integers.  Additional work may become necessary to ensure
    the entries are never larger than the dimension of the group table.

--- checkAssociativeProperty.m
+++ checkAssociativeProperty.m
@@ -8,13 +8,22 @@ function checkAssociativeProperty (group_table_file)
   
   group_size = length (group_table(1,:))
   
+  # Group table should be a square matrix of dimension n, where n is the
+  # number of elements in the group
   if (group_size != length (group_table(:,1)))
     printf("The group table is incomplete. Please review the entries and ensure they are all present and correct.\n")
     return
   endif
   
-  # TODO : Build in a test case here to ensure that the provided group table is 
-  # a square matrix
+  # Group table entries should be positive integers
+  for i = 1:group_size
+    for j = 1:group_size
+      if (!isinteger(group_table(i,j)) || !gt(group_table(i,j),0))
+        printf("Group table entries should be positive integers, not including 0\n")
+        return
+      endif
+    endfor
+  endfor
   
   isAssociative = true
   
